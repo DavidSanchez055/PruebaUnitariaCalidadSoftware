52d84e404faabd30035025267e2586501895216f
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2024-04-24 18:58:04 -0500

    Corrección correo.test.js

--- correo.js
+++ correo.js
@@ -1,5 +1,10 @@
 function obtenerCorreo (CorreoElectronico){
     return CorreoElectronico
 }
+function validarCorreoElectronico(correo) {
+    const patron = /^[^@]+@[^@]+\.[^@]+$/;
+    return patron.test(correo);
+  }
 
 module.exports = obtenerCorreo; 
+module.exports = validarCorreoElectronico;
--- correo.test.js
+++ correo.test.js
@@ -1,18 +1,13 @@
 const validacion = require('./correo');
 
-function validarCorreoElectronico(correo) {
-  const patron = /^[^@]+@[^@]+\.[^@]+$/;
-  return patron.test(correo);
-}
-
 test('Validar correo electrónico válido', () => {
-  expect(validacion("davidf.sanchezm@gmail.com")).toBe("davidf.sanchezm@gmail.com");
-  expect(validacion("pipesan55@gmail.com")).toBe("pipesan55@gmail.com");
+  expect(validacion("davidf.sanchezm@gmail.com")).toBe(true);
+  expect(validacion("pipesan55@gmail.com")).toBe(true);
 });
 
 test('Validar correo electrónico no válido', () => {
-  expect(validarCorreoElectronico("davidf.sanchezm@gmail.com")).toBe(true);
-  expect(validarCorreoElectronico("pipesan55@gmail.com")).toBe(true);
-  expect(validarCorreoElectronico("correoinvalido@dominio")).toBe(false); 
-  expect(validarCorreoElectronico("correo@invalido")).toBe(false); 
+  expect(validacion("davidf.sanchezm@gmail.com")).toBe(true);
+  expect(validacion("pipesan55@gmail.com")).toBe(true);
+  expect(validacion("correoinvalido@dominio")).toBe(false); 
+  expect(validacion("correo@invalido")).toBe(false); 
 });
